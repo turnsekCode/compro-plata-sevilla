6a6c3682718c4ba6310c0b0f35d6ef664cfa09ff
abraham <abraham.turnsek@gmail.com>
2023-03-30 17:23:05 +0200

    cambio script cookie

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,20 +4,22 @@ import Script from "next/script";
 export default function Document() {
   return (
     <Html lang="es">
-      <Script
+      <script
+        defer
         id="Cookiebot"
         src="https://consent.cookiebot.com/uc.js"
         data-cbid="66a968ad-70de-4703-81be-84844ed28395"
         data-blockingmode="auto"
         type="text/javascript"
-        property="lazyOnload"
-      ></Script>
-      <Script
+        //property="lazyOnload"
+      ></script>
+      <script
+        defer
         id="CookieDeclaration"
         src="https://consent.cookiebot.com/66a968ad-70de-4703-81be-84844ed28395/cd.js"
         type="text/javascript"
-        property="lazyOnload"
-      ></Script>
+        // property="lazyOnload"
+      ></script>
       <link
         rel="stylesheet preload prefetch"
         href="https://api.mapbox.com/mapbox-gl-js/v2.8.1/mapbox-gl.css"
